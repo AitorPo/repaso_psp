1c8c6c367af435af40962567dcb6a3c0e76fa115
AitorPo <aitorpoquetgin@gmail.com>
2021-01-21 09:59:30 +0100

    Primer commit

---
+++ src/main/java/com/svalero/App.java
@@ -0,0 +1,26 @@
+package com.svalero;
+
+import com.svalero.utils.R;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(R.getUi("carrera.fxml"));
+        loader.setController(new AppController());
+        Parent root = loader.load();
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+}
---
+++ src/main/java/com/svalero/AppController.java
@@ -0,0 +1,14 @@
+package com.svalero;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class AppController {
+
+
+
+    @FXML
+    public void iniciarCarrera(ActionEvent event){
+
+    }
+}
